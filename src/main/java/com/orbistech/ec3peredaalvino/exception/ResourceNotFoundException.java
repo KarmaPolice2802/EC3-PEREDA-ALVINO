894e66f7c121dc838f52a13833df2fd516fa1247
Gianmarko Pereda Zarate <gianmarko@MacBook-Pro-de-Gianmarko.local>
2025-03-15 14:03:52 -0500

    Manejo de excepciones

--- src/main/java/com/orbistech/ec3peredaalvino/exception/ResourceNotFoundException.java
+++ src/main/java/com/orbistech/ec3peredaalvino/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.orbistech.ec3peredaalvino.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
