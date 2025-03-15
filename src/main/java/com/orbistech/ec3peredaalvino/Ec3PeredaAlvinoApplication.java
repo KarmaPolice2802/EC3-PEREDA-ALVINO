5f713bb1b269cd6da1ad282908428e89c828ccb2
Gianmarko Pereda Zarate <gianmarko@MacBook-Pro-de-Gianmarko.local>
2025-03-15 11:07:58 -0500

    first commit

---
+++ src/main/java/com/orbistech/ec3peredaalvino/Ec3PeredaAlvinoApplication.java
@@ -0,0 +1,13 @@
+package com.orbistech.ec3peredaalvino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3PeredaAlvinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec3PeredaAlvinoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orbistech/ec3peredaalvino/Ec3PeredaAlvinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbistech.ec3peredaalvino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3PeredaAlvinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
